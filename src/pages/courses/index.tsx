afdfc859cafecafb35d627cb40082a0bd3b5813e
Hieu Nguyen <thanhhieunguyen183@gmail.com>
2025-09-16 10:19:17 +0700

    feat: Separate Sort and Filter buttons

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -1,8 +1,9 @@
 import CoursesPanel from 'containers/CoursesPanel';
+import hooks from 'containers/Dashboard/hooks';
 import React from 'react';
 
-const Courses = () => (
-  <div><CoursesPanel /></div>
-);
-
+const Courses = () => {
+  hooks.useInitializeDashboard();
+  return <div><CoursesPanel /></div>;
+};
 export default Courses;
