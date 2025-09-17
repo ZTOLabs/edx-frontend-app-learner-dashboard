d5c6f73b640f831dfe643de499835ab6e5c8c105
Hieu Nguyen <thanhhieunguyen183@gmail.com>
2025-09-17 10:29:25 +0700

    feat: Change notification into dropdown menu trigger

--- src/shared/Components/Layouts/AppSidebar/index.tsx
+++ src/shared/Components/Layouts/AppSidebar/index.tsx
@@ -1,5 +1,4 @@
 import {
-  Bell01,
   ClipboardCheck,
   HomeLine,
   Globe01,
@@ -24,19 +23,13 @@ import messages from '../../../../messages';
 import Items from './Navigation/item';
 import AppLogo from '../AppLogo';
 import UserProfile from './UserProfile';
+import Notification from './Notification';
 
 const getBaseRoute = (pathname: string): string => {
   const segments = pathname.split('/').filter(Boolean);
 
   return segments[0] || '';
 };
-
-const appSettingItems = [
-  {
-    url: '/notification',
-    icon: Bell01,
-  },
-];
 
 const AppSidebar = ({ ...props }: React.ComponentProps<typeof Sidebar>) => {
   const location = useLocation();
@@ -96,9 +89,7 @@ const AppSidebar = ({ ...props }: React.ComponentProps<typeof Sidebar>) => {
 
       <SidebarFooter className="tw-flex tw-flex-col tw-gap-4 !tw-p-0 tw-items-center">
         <SidebarMenu className="tw-list-none tw-flex tw-flex-col tw-pl-0 tw-mb-0">
-          {appSettingItems.map((item) => (
-            <Items item={item} key={item.url} />
-          ))}
+          <Notification />
         </SidebarMenu>
 
         <SwitchContainer />
