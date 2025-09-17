ae1302161ed365547f60b7c4d89f6f96f575e155
Hieu Nguyen <thanhhieunguyen183@gmail.com>
2025-09-17 10:26:03 +0700

    refactor: Remove header and footer instead of comment

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,6 @@ export const App = () => {
       </Helmet>
       <div>
         <AppWrapper>
-          {/* <LearnerDashboardHeader /> */}
           <main id="main">
             {hasNetworkFailure ? (
               <Alert variant="danger">
@@ -101,7 +100,6 @@ export const App = () => {
             )}
           </main>
         </AppWrapper>
-        {/* <FooterSlot /> */}
       </div>
     </>
   );
